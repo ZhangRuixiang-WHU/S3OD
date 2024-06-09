d25ca790ea5e10a2e9aa4e63cec1d3273f161e9f
ZhangRuixiang-WHU <zhangruixiang@whu.edu.cn>
2024-06-10 00:58:00 +0800

    Initial commit

--- tools/dataset/semi_dotav2.sh
+++
@@ -1,7 +0,0 @@
-
-OFFSET=$RANDOM
-for percent in 1 5 10; do
-    for fold in 1 2 3 4 5; do
-        python tools/dataset/semi_dotav2.py --percent ${percent} --seed ${fold} --seed-offset ${OFFSET}
-    done
-done
